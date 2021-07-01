ed027d9b36728989ea0a821ca3ae48ef6edf09e0
Rapahel <raphael.stascheit@study.hs-duesseldorf.de>
2021-07-01 18:25:49 +0200

    added some more bad chars

--- purge_bad_characters_in_filenames.py
+++ purge_bad_characters_in_filenames.py
@@ -12,27 +12,35 @@ import os
 from tqdm import tqdm
 
 # list from where to start?
-path = '.'
+path = input("Directory that will be searched in recursively:\n")
+
 # list all files recursively
 list_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(path) for f in filenames]
 
 # those will be purged
-bad_chars = ('\n', '\r', ',')
+bad_chars = ("\n", "\r", ",", ";", ":")
+
+count_issues = 0
 
 # cycle through found files
 for path_src in tqdm(list_files):
     # make a copy
     path_dst = path_src
+    found_issues = []
 
     # cycle through bad chars
     for bad_char in bad_chars:
         if bad_char in path_src:
             # if a wild bad char appeared, replace it with an empty string
-            path_dst = path_src.replace(bad_char, '')
+            found_issues.append(bad_char)
+            path_dst = path_src.replace(bad_char, "")
 
     # if a bad char appeared, those should be different now
     if path_dst != path_src:
         # tell the user what's happening
-        print(f"Renaming: `{os.path.basename(path_dst)}`")
+        print(f"Renaming: `{os.path.basename(path_dst)}` (Due to {found_issues})")
         # rename file
         os.rename(path_src, path_dst)
+        count_issues += 1
+
+print(f"\nRenamed {count_issues} files.")
